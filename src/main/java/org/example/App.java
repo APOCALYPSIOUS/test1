9f9787f5b658c06f5644196f1cabb846cfc09352
APOCALYPSIOUS <85230304+APOCALYPSIOUS@users.noreply.github.com>
2023-05-08 23:19:55 +0100

    hey2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello " );
     }
+    //hi im a comment
     //hi im a comment
 }
